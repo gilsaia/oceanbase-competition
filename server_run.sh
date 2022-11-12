bd3114de97b487a26946c9e1421f38e75a9af856
gilsaia <794433219@qq.com>
2022-11-12 11:49:42 +0800

    fix:server choose debug/release

--- server_run.sh
+++ server_run.sh
@@ -2,8 +2,23 @@
 
 # data file
 data_file=$HOME/data
-# obd mirror create -p path
-mirror_create_path=$PWD/build_release/usr/local/
+
+
+case $1 in
+    release)
+        # obd mirror create -p path
+        mirror_create_path=$PWD/build_release/usr/local/
+        ;;
+    debug)
+        # obd mirror create -p path
+        mirror_create_path=$PWD/build_debug/usr/local/
+        ;;
+    *)
+        # obd mirror create -p path
+        mirror_create_path=$PWD/build_debug/usr/local/
+        ;;
+esac
+
 
 # yaml file
 yaml_file=$PWD/final_2022.yaml
