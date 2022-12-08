2d343aa620d5bc1b9a9c27f27a8c4850b39119f5
linzy <444210421@qq.com>
2022-12-08 21:47:34 +0800

    test

--- src/sql/engine/cmd/ob_load_data_direct_demo.h
+++ src/sql/engine/cmd/ob_load_data_direct_demo.h
@@ -17,8 +17,8 @@ namespace oceanbase
 namespace sql
 {
 #define READ_THREAD_NUM 1
-#define CAST_THREAD_NUM 4
-#define WRITE_THREAD_NUM 3
+#define CAST_THREAD_NUM 3
+#define WRITE_THREAD_NUM 4
 
 class ObLoadDataBuffer
 {
@@ -308,7 +308,7 @@ private:
 
 class ObWriteThreadPool : public share::ObThreadPool
 {
-  static const int64_t MEM_BUFFER_SIZE = (1LL << 28); // 1G
+  static const int64_t MEM_BUFFER_SIZE = (1LL << 30); // 1G
   static const int64_t FILE_BUFFER_SIZE = (2LL << 20); // 2M
   static const int64_t WRITE_PARALLEL_DEGREE = WRITE_THREAD_NUM;
 public:
